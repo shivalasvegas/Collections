09827bdbfe653da0b3f98ce51dbbc8468dc6e9df
shivalasvegas <shiva.king@academytrainee.com>
2020-06-26 10:18:16 +0100

    Initial commit

---
+++ src/com/qa/collections/Collections.java
@@ -0,0 +1,60 @@
+package com.qa.collections;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		hundredArray();
+		hundredArrayList();
+		multipliedArrayList();
+
+	}
+	
+	static void hundredArray() {
+		int[] hunArray = new int[100];
+		int num = 0;
+		
+		for (int i = 0; i<hunArray.length; i++) {
+			num = i+1;
+			hunArray[i] = num;
+			//System.out.println("Adding ... " + i);
+		}
+	
+		//System.out.println(Arrays.toString(hunArray));
+		System.out.println("Added to array from " + hunArray[0] + " to " + hunArray[99]);
+	}
+	
+	public static ArrayList<Integer> hunArrayList = new ArrayList<Integer>();
+	 
+	static void hundredArrayList() {
+		//ArrayList<Integer> hunArrayList = new ArrayList<Integer>();
+		int num = 0;
+		
+		for (int i = 0; i<900; i++) {
+			num = 100 + i;
+			hunArrayList.add(num);
+			
+		}
+	
+		System.out.println("Added to arraylist from " + hunArrayList.get(0) + " to " + hunArrayList.get(899));
+	}
+	
+	static void multipliedArrayList() {
+		ArrayList<Integer> multiArrayList = new ArrayList<Integer>();
+		int num = 0;
+		
+		for (int i = 0; i<hunArrayList.size(); i++) {
+			num = hunArrayList.get(i) * 10;
+			multiArrayList.add(num);
+			
+		}
+	
+		System.out.println("Added to arraylist from " + multiArrayList.get(0) + " to " + multiArrayList.get(899));
+	}
+	
+	
+}
+
+
